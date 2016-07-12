2589d4e10c239f6a1abd32c71546ad23a9074614
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-07-12 19:02:49 +0100

    +0.1.1 in progress.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,28 +1,43 @@
-'use strict';
+'use strict'
 
-const gulp = require('gulp'),
-     clean = require('gulp-rimraf'),
-       deb = require('./');
+const gulp = require('gulp')
+const clean = require('gulp-rimraf')
+const sequence = require('gulp-sequence')
+const standard = require('gulp-standard')
+const deb = require('./')
 
-gulp.task('deb', function() {
-	return gulp.src(['.gitignore','.npmignore'])
-	.pipe(deb({
-		package: 'demo',
-		version: '0.1-1',
-		section: 'base',
-		priority: 'optional',
-		architecture: 'i386',
-		maintainer: 'Mr. Apt <apt@nowhere.tld>',
-		description: 'A dummy package',
-		_target: 'opt/demo',
-		_out: 'dist',
-		_verbose: true
-	 }));
-});
+gulp.task('deb', function () {
+  return gulp.src(['.gitignore', '.npmignore'])
+  .pipe(deb({
+    package: 'demo',
+    version: '0.1-1',
+    section: 'base',
+    priority: 'optional',
+    architecture: 'i386',
+    maintainer: 'Mr. Apt <apt@nowhere.tld>',
+    description: 'A dummy package',
+    _target: 'opt/demo',
+    _out: 'dist',
+    _verbose: true
+  }))
+})
 
-gulp.task('clean', function() {
-	return gulp.src('dist')
-	.pipe(clean());
-});
+gulp.task('standard', function () {
+  return gulp.src('*.js')
+  .pipe(standard())
+  .pipe(standard.reporter('default', {
+    breakOnError: true
+  }))
+})
 
-gulp.task('default', ['deb'], function(){});
+gulp.task('nodeunit', function () {
+  // TODO!
+})
+
+gulp.task('clean', function () {
+  return gulp.src('dist')
+  .pipe(clean())
+})
+
+gulp.task('default', ['deb'])
+gulp.task('test', sequence('standard', 'deb', 'assert'))
