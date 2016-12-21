20c658728acab2241b2e8415ff2ad4d565af2fb0
Olaf Radicke <o.radicke@meteocontrol.de>
2016-12-21 17:55:58 +0100

    Fix test

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 
 exports.debian = function (test) {
   test.expect(1)
-  const actual = fs.readFileSync('dist/demo_0.1-1/DEBIAN/control').toString()
+  const actual = fs.readFileSync('dist/demo_0.1-1_i386/DEBIAN/control').toString()
   const expected = fs.readFileSync('test/control').toString()
   test.equal(actual, expected, 'CONTROL file created should equal sample.')
   test.done()
