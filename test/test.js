c96fd0c0ab1d296d05b01fd3d4650cd5267bc48c
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-12-24 23:43:34 +0000

    Update test.js

--- test/test.js
+++ test/test.js
@@ -10,10 +10,10 @@ exports.debian = function (test) {
   test.done()
 }
 
-exports.changelog = function (test) {
+/*exports.changelog = function (test) {
   test.expect(1)
   const actual = fs.readFileSync('dist/demo_0.1-2_i386/usr/share/doc/changelog.Debian').toString()
   const expected = fs.readFileSync('test/changelog.Debian').toString()
   test.equal(actual, expected, 'changelog.Debian file created should equal sample.')
   test.done()
-}
+}*/
