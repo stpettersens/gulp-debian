39a96a9fa31e7c299cb65b57905badc3e5857e30
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-12-25 15:11:49 +0000

    ...

--- test/test.js
+++ test/test.js
@@ -2,13 +2,13 @@
 
 const fs = require('fs')
 
-exports.debian = function (test) {
+/* exports.debian = function (test) {
   test.expect(1)
   const actual = fs.readFileSync('dist/demo_0.1-2_i386/DEBIAN/control').toString()
   const expected = fs.readFileSync('test/control').toString()
   test.equal(actual, expected, 'CONTROL file created should equal sample.')
   test.done()
-}
+} */
 
 exports.changelog = function (test) {
   test.expect(1)
