324b76b706bdb9e8b0b840ad693f1b6dd12a4884
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-12-25 12:40:18 +0000

    fix?

--- test/test.js
+++ test/test.js
@@ -12,7 +12,7 @@ exports.debian = function (test) {
 
 exports.changelog = function (test) {
   test.expect(1)
-  const actual = fs.readFileSync('dist/demo_0.1-2_i386/usr/share/doc/changelog.Debian').toString()
+  const actual = fs.readFileSync('dist/demo_0.1-2_i386/usr/share/doc/demo/changelog.Debian').toString()
   const expected = fs.readFileSync('test/changelog.Debian').toString()
   test.equal(actual, expected, 'changelog.Debian file created should equal sample.')
   test.done()
