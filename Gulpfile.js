06627e89711e83f5e97e3be0e9cdde3b43af7b62
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-12-29 21:08:13 +0000

    0.1.4 ready?

--- Gulpfile.js
+++ Gulpfile.js
@@ -23,8 +23,8 @@ gulp.task('deb', function () {
     architecture: 'i386',
     maintainer: 'Mr. Apt <apt@nowhere.tld>',
     description: 'A dummy package',
-    preinst: [ 'cat opt/demo/.gitignore' ],
-    postinst: [ 'cat /opt/demo/.npmignore' ],
+    preinst: [ 'echo "hello from dummy package"' ],
+    postinst: [ 'cat -n /opt/demo/.npmignore' ],
     changelog: [
       {
         version: '0.1-2',
