19221b64f8698dbf3a34d675e5c6b236d2063d9e
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2017-01-04 18:24:23 +0000

    -chalk for json2yaml.

--- json2yaml.js
+++ json2yaml.js
@@ -4,7 +4,6 @@
 
 const YAML = require('js-yaml')
 const fs = require('fs-extra')
-const chalk = require('chalk')
 
 module.exports.JSON2YAML = function (jsonf) {
   let yamlf = jsonf.split('.json')
@@ -12,6 +11,6 @@ module.exports.JSON2YAML = function (jsonf) {
   const json = fs.readJSONSync(jsonf)
   json.package = `${json.package.replace(/json/, 'yaml')}`
   const yaml = YAML.safeDump(json)
-  console.log(chalk.magenta(yaml))
+  console.log(yaml)
   fs.writeFileSync(yamlf, yaml)
 }
